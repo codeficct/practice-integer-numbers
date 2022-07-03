e6e7347c4991e388a04dab91920973ddf8ec731b
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-02 20:02:38 -0400

    feat: add methods to resize form

--- Form1.vb
+++ Form1.vb
@@ -1,8 +1,44 @@
 ﻿'*** Practice of integer numbers ***
+Imports System.Runtime.InteropServices
+
 Public Class Form1
     Private leftBorderBtn As Panel
     Private currentBtn As Button
-
+    Private borderSize As Integer = 0
+    'RESIZE OF FORM - CHANGE SIZE'
+    Dim cGrip As Integer = 10
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        If (m.Msg = 132) Then
+            Dim pos As Point = New Point((m.LParam.ToInt32 And 65535), (m.LParam.ToInt32 + 16))
+            pos = Me.PointToClient(pos)
+            If ((pos.X _
+                    >= (Me.ClientSize.Width - cGrip)) _
+                    AndAlso (pos.Y _
+                    >= (Me.ClientSize.Height - cGrip))) Then
+                m.Result = CType(17, IntPtr)
+                Return
+            End If
+        End If
+        MyBase.WndProc(m)
+    End Sub
+    '----------------DRAW RECTANGLE / EXCLUDE CORNER PANEL' 
+    Dim sizeGripRectangle As Rectangle
+    Dim tolerance As Integer = 15
+    Protected Overrides Sub OnSizeChanged(ByVal e As EventArgs)
+        MyBase.OnSizeChanged(e)
+        Dim region = New Region(New Rectangle(0, 0, Me.ClientRectangle.Width, Me.ClientRectangle.Height))
+        sizeGripRectangle = New Rectangle((Me.ClientRectangle.Width - tolerance), (Me.ClientRectangle.Height - tolerance), tolerance, tolerance)
+        region.Exclude(sizeGripRectangle)
+        Me.PanelFormContainer.Region = region
+        Me.Invalidate()
+    End Sub
+    '----------------COLOR Y GRIP DE RECTANGULO INFERIOR'
+    'Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+    '    Dim blueBrush As SolidBrush = New SolidBrush(Color.FromArgb(34, 33, 74))
+    '    e.Graphics.FillRectangle(blueBrush, sizeGripRectangle)
+    '    MyBase.OnPaint(e)
+    '    ControlPaint.DrawSizeGrip(e.Graphics, Color.Transparent, sizeGripRectangle)
+    'End Sub
     'Constructor
     Public Sub New()
         ' Esta llamada es exigida por el diseñador.
@@ -95,23 +131,50 @@ Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles StudentBtn.Click
         DisableButton()
         PanelStudent.Visible = True
-        StudentBtn.BackColor = Color.FromArgb(37, 36, 81)
+        StudentBtn.BackColor = Color.FromArgb(34, 33, 74)
         StudentBtn.ForeColor = Color.FromArgb(130, 83, 215)
         leftBorderBtn.Visible = False
     End Sub
+    'Drag Form
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal WParam As Integer, ByVal IParam As Integer)
+    End Sub
+    Private Sub PanelTitleBar_MouseDown(sender As Object, e As MouseEventArgs) Handles PanelTitleBar.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If WindowState = FormWindowState.Normal Then
+            WindowState = FormWindowState.Maximized
+        Else
+            WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
 
     Public Function EvenAndOdd(number As Integer) As Boolean
-    Return number Mod 2 = 0
+        Return number Mod 2 = 0
     End Function
 
     '<-- 1. Accumuate terms according to the formula with odd digits -->
-    Public Function AccumulateOddDigits(number As Integer) As String
-    Dim result As String = ""
-    Dim digit As Byte
-    While number > 0
-        digit = number Mod 10
-        number \= 10
+    'Public Function AccumulateOddDigits(number As Integer) As String
+    '    Dim result As String = ""
+    '    Dim digit As Byte
+    '    While number > 0
+    '        digit = number Mod 10
+    '        number \= 10
 
-    End While
-    End Function
+    '    End While
+    'End Function
 End Class
